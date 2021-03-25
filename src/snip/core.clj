6cafd941ba55e35e50c35630eb00a54e81db3980
Brad <brad.clj@gmail.com>
2021-03-25 08:49:20 -0700

    primes

--- src/snip/core.clj
+++ src/snip/core.clj
@@ -47,3 +47,69 @@
   (let [[ds av] (ds-find ds a)
         [ds bv] (ds-find ds b)]
     (assoc ds bv av)))
+
+;; primes
+
+(defn prime?
+  [n]
+  (cond
+    (<= n 3)
+    (> n 1)
+
+    (or (zero? (rem n 2))
+        (zero? (rem n 3)))
+    false
+
+    :else
+    (loop [i 5]
+      (if (<= (* i i) n)
+        (if (or (zero? (rem n i))
+                (zero? (rem n (+ i 2))))
+          false
+          (recur (+ i 6)))
+        true))))
+
+(def p-max (-> 1000000000 Math/sqrt inc long))
+
+(def primes
+  (filter prime? (range 2 (inc p-max))))
+
+;; not as fast
+#_
+(def primes
+  (let [t (boolean-array (inc p-max))
+        f (fn f
+            [i]
+            (doseq [i (->> (iterate #(+ % i) i)
+                           (take-while #(<= % p-max)))]
+              (aset t i true))
+            (let [i (some (fn [i]
+                            (if (not (aget t i)) i))
+                          (range (inc i) (inc p-max)))]
+              (if (some? i)
+                (lazy-seq (cons i (f i))))))]
+    (lazy-seq (cons 2 (f 2)))))
+
+(defn factor
+  [n]
+  (if (pos? n)
+    (-> (reduce (fn [[m n] p]
+                  (cond
+                    (= n 1)
+                    (reduced [m n])
+
+                    (prime? n)
+                    (reduced [(assoc m n 1) 1])
+
+                    :else
+                    (let [[n i] (loop [n n
+                                       i 0]
+                                  (if (zero? (rem n p))
+                                    (recur (quot n p) (inc i))
+                                    [n i]))]
+                      (if (pos? i)
+                        [(assoc m p i) n]
+                        [m n]))))
+                [{} n]
+                primes)
+        first)))
